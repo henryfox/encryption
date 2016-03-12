79154645aea3c12fe2b15355fd29cbeb145422e6
henryfox <henryfox831@gmail.com>
2016-03-12 15:38:53 -0800

    convert pass phrase into numbers

--- encrypt.py
+++ encrypt.py
@@ -5,8 +5,6 @@ import math
 
 alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", ",", ":", ";", "'", '"', "<", ">", "?", " "]
 
-alphabet_cap = []
-
 passpharse = sys.argv[2][2:]
 path = sys.argv[3]
 
@@ -21,13 +19,16 @@ if sys.argv[1] == "-e":
 	nopll = nopl.split(".")
 	pass_phrase_full = []
 	y = 0
-	pass_phrase_num_list
+	pass_phrase_list_nums = []
 	
 	if len(nopll) == 2:
 		nopl_frac = (float(nopl)).as_integer_ratio()
 		if nopl_frac[1] < fl:
 			nopl_frac_bn = math.floor(fl / nopl_frac[1])
 			nopl_frac_tn = math.floor(nopl_frac[0] * nopl_frac_bn)
+		else:
+			nopl_frac_bn = math.floor(nopl_frac[1] / fl)
+			nopl_frac_tn = math.floor(nopl_frac[0] / nopl_frac_bn)
 	
 	passpharse_list = list(passpharse)
 	
@@ -36,3 +37,13 @@ if sys.argv[1] == "-e":
 			y = 0
 		pass_phrase_full.append(passpharse_list[y])
 		y += 1
+	for x in pass_phrase_full:
+		y = 0
+		z = 0
+		for y in alphabet:
+			z += 1
+
+			if y == x:
+				break
+		pass_phrase_list_nums.append(z)
+	print pass_phrase_list_nums
