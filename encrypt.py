9948476806c7b37b6abe17dc873bf11c261871ce
henryfox <henryfox831@gmail.com>
2016-03-12 14:49:38 -0800

    genorate full pass phrase

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,38 @@
+from __future__ import division
 import sys
 from rand import rand
+import math
 
-print rand(10)
+alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", ",", ":", ";", "'", '"', "<", ">", "?", " "]
+
+alphabet_cap = []
+
+passpharse = sys.argv[2][2:]
+path = sys.argv[3]
+
+with open(path[2:]) as fi:
+	fi = fi.read()
+
+if sys.argv[1] == "-e":
+	fl = len(fi)
+	num = rand(fl)
+	pl = len(passpharse)
+	nopl = str(fl / pl)
+	nopll = nopl.split(".")
+	pass_phrase_full = []
+	y = 0
+	pass_phrase_num_list
+	
+	if len(nopll) == 2:
+		nopl_frac = (float(nopl)).as_integer_ratio()
+		if nopl_frac[1] < fl:
+			nopl_frac_bn = math.floor(fl / nopl_frac[1])
+			nopl_frac_tn = math.floor(nopl_frac[0] * nopl_frac_bn)
+	
+	passpharse_list = list(passpharse)
+	
+	for x in range(0, fl):
+		if y >= pl:
+			y = 0
+		pass_phrase_full.append(passpharse_list[y])
+		y += 1
