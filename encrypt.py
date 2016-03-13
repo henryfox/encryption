2a2c147d1c9435a937d4e6083c98855bc6cb267d
henryfox <henryfox831@gmail.com>
2016-03-12 17:14:50 -0800

    add code that writes to the file that needs to be encrypted

--- encrypt.py
+++ encrypt.py
@@ -5,10 +5,10 @@ import math
 
 alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", ",", ":", ";", "'", '"', "<", ">", "?", " "]
 
-passpharse = sys.argv[2][2:]
-path = sys.argv[3]
+passpharse = sys.argv[2][1:]
+path = sys.argv[3][1:]
 
-with open(path[2:]) as fi:
+with open(path) as fi:
 	fi = fi.read()
 
 if sys.argv[1] == "-e":
@@ -18,6 +18,7 @@ if sys.argv[1] == "-e":
 	pass_phrase_full = []
 	y = 0
 	pass_phrase_list_nums = []
+	oo = False
 	
 	passpharse_list = list(passpharse)
 	
@@ -62,6 +63,19 @@ if sys.argv[1] == "-e":
 	for x in range(0, len(num_list)):
 		fi_list_nums_final.append(str(int(num_list[x]) + int(fi_list_nums[x])))
 	fi_nums_final = " ".join(fi_list_nums_final)
-	
-	print fi_nums_final
-	print num_final
+
+	try:
+		save = sys.argv[4]
+		if save == "-d":
+			print num_final + " file " + fi_nums_final
+		if save == "-s":
+			try:
+				save_location = argv[5]
+
+				with open(save_location,"w") as fil:
+					fil.write(num_final + " file " + fi_nums_final)
+			except:
+				"you did not enter a save location or the save location you enterd is not valid"
+	except:
+		with open(path,"w") as fil:
+			fil.write(num_final + " file " + fi_nums_final)
